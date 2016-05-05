49c99e2446c4a3996598e8659d36e176ab3d30bb
Krittika DSilva <krittika.dsilva@gmail.com>
2016-05-05 16:30:18 -0700

    finished for the day

--- app/scripts/QRcapture.js
+++
@@ -1,6 +0,0 @@
-var video = document.getElementById('camera');
-QCodeDecoder().decodeFromCamera(video, function(er,res){
-  console.log(res);                     
-  // TODO   
-});
-
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,34 @@
-console.log('\'Allo \'Allo!');
+var video = document.getElementById('camera');
+var decodedMessage = ""
+QCodeDecoder().decodeFromCamera(video, function(er,res){
+  decodedMessage = res
+  console.log(res);                     
+});
+
+var ros = new ROSLIB.Ros({
+  url : 'ws://localhost:9090'
+});
+
+ros.on('connection', function() {
+  console.log('Connected to websocket server.');
+});
+
+var qr_code_topic = new ROSLIB.Topic({
+  ros : ros,
+  name : '/jeeves_qr_code',
+  messageType : 'std_msgs/String'
+});
+
+var message = new ROSLIB.Message({
+  linear : {
+    x : 0.1,
+    y : 0.2,
+    z : 0.3
+  },
+  angular : {
+    x : -0.1,
+    y : -0.2,
+    z : -0.3
+  }
+});
+qr_code_topic.publish(twist);
