1fa46ceb166207ea11c49996674cb8b4a8ccb507
Mallika Mathur <mkmathur@cs.uw.edu>
2016-05-12 12:32:36 -0700

    Reorganize so that we can use python server instead of grunt

---
+++ scripts/receiveOrder.js
@@ -0,0 +1,80 @@
+console.log('Hello');
+var ros = new ROSLIB.Ros({
+    url : 'ws://localhost:9090'
+});
+
+ros.on('connection', function() {
+    console.log('Connected to websocket server.');
+});
+
+var order_topic = new ROSLIB.Topic({
+    ros : ros,
+    name : '/jeeves_order',
+    messageType : 'jeeves/Order'
+});
+
+var OrderListItem = React.createClass({
+
+    render: function() {
+        return (
+            <li className='order-list-item'>
+                <span className='order-name'>{this.props.name}</span>
+                <span className='order-location'>{this.props.location}</span>
+                <span className='order-food-type'>{this.props.food_type}</span>
+                <form>
+                    <button>Print</button>
+                </form>
+            </li>
+        );
+    }
+
+});
+
+var OrderList = React.createClass({
+    render: function() {
+        var orderNodes = this.props.orders.map(function (order) {
+            return (
+                <OrderListItem name={order.name} location={order.location} food_type={order.food_type}>
+                </OrderListItem>
+            );
+        });
+
+        return (
+            <ul className='order-list'>
+                { orderNodes }
+            </ul>
+        );
+    }
+});
+
+var OrderApp = React.createClass({
+    getInitialState: function() {
+        return { orders: [] };
+    },
+
+    componentWillMount: function() {
+        var that = this;
+        order_topic.subscribe(function(message) {
+            // THAT because javascript
+            console.log(that)
+            that.state.orders.push(message);
+            that.setState({
+                orders: that.state.orders
+            });
+        });
+    },
+
+    componentWillUnmount: function() {
+        order_topic.unsubscribe();
+    },
+
+    render: function() {
+        return (
+            <div className='orderapp'>
+                <OrderList orders={this.state.orders}/>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(<OrderApp/>, document.body);
