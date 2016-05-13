aa5ca7b420274b39569955dbd7df634ce8e4fd8b
Mallika Mathur <mkmathur@cs.uw.edu>
2016-05-13 13:33:44 -0700

    Add phone number to order form.

--- scripts/payOrder.js
+++ scripts/payOrder.js
@@ -24,20 +24,24 @@ function processOrder(){
   // TODO: what if there's an '?
   var url = document.location.href;
   var name = getParameterByName('name', url);
+  var phoneNumber = getParameterByName('phone', url);
   var location = getParameterByName('location', url);
   var food = getParameterByName('food', url);
 
-  console.log(name);
-  console.log(location);
-  console.log(food);
+//  console.log(name);
+//  console.log(location);
+//  console.log(food);
 
-  var payload = name + "," + location + "," + food;
+  var orderData = [name, phoneNumber, location, food]
+  var payload = orderData.join()
   var order = new ROSLIB.Message( {
       name : name,
+      phone_number : phoneNumber,
       location : location,
       food_type : food
   });
   order_topic.publish(order);
+  ros.close();
 
   // new QRCode(document.getElementById("qrcode"), payload);
   document.location.href='prepare.html';
--- scripts/submitOrder.js
+++ scripts/submitOrder.js
@@ -1,35 +1,19 @@
-var ros = new ROSLIB.Ros({
-    url : 'ws://localhost:9090'
-});
-
-ros.on('connection', function() {
-    console.log('Connected to websocket server.');
-});
-
-var order_topic = new ROSLIB.Topic({
-    ros : ros,
-    name : '/jeeves_order',
-    messageType : 'jeeves/Order'
-});
-
-
 function processOrder(){
   // TODO: what if there's an '?
-  var name = document.getElementById("userName").value;
+  var name = document.getElementById("name").value;
+  var phoneNumber = document.getElementById("phoneNumber").value;
   var locationDrop = document.getElementById("dropdownLocation");
   var foodDrop = document.getElementById("dropdownFood");
 
   var location = locationDrop[locationDrop.selectedIndex].id;
   var food = foodDrop[foodDrop.selectedIndex].id
 
-  console.log(name)
-  console.log(location)
-  console.log(food)
+    //  console.log(name)
+    //  console.log(location)
+    //  console.log(food)
 
-  var newUrl = '/credit.html?name=' + name + '&location=' + location + '&food='+food;
+    var newUrl = '/credit.html?name=' + name + '&phone=' + phoneNumber + '&location=' + location + '&food='+food;
   document.location.href=newUrl;
 };
-
-
 
 var button = document.getElementById("submitOrder").onclick = processOrder;
