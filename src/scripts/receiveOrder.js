e57ef85ca0298d64c1f71240065233e1931fdf0c
Mallika Mathur <mallika@mkmathur.com>
2016-06-01 22:29:05 -0700

    Implement archive button. Move print button to table header.

--- src/scripts/receiveOrder.js
+++ src/scripts/receiveOrder.js
@@ -15,64 +15,42 @@ injectTapEventPlugin();
 ordersRef = new Firebase("https://jeeves-server.firebaseio.com/orders");
 notifsRef = new Firebase("https://jeeves-server.firebaseio.com/notifs");
 
-var OrderListItem = React.createClass({
-
-    onPrintButtonClick: function() {
-      var order = this.props.order;
-      var data = [order.name, order.phone_number, order.location, order.food_type];
-      var message = data.join(',');
-      this.props.updateQR(message);
-      this.send_notification();
-      window.print();
-    },
-
-    send_notification: function () {
-      message = {
-        to: this.props.order.phone_number,
-        body: this.props.order.name + ", your delivery of " + this.props.order.food_type + " is on its way!"
-      };
-      console.log(message);
-      notifsRef.push(message);
-    },
-
-    render: function() {
-        return (
-            <MaterialUi.TableRow className='order-list-item'>
-                <MaterialUi.TableRowColumn className='order-name'>{this.props.order.name}</MaterialUi.TableRowColumn>
-                <MaterialUi.TableRowColumn className='order-location'>{this.props.order.location}</MaterialUi.TableRowColumn>
-                <MaterialUi.TableRowColumn className='order-food-type'>{this.props.order.food_type}</MaterialUi.TableRowColumn>
-                <MaterialUi.TableRowColumn className='order-phone-number'>{this.props.order.phone_number}</MaterialUi.TableRowColumn>
-                <MaterialUi.TableRowColumn>
-                    <MaterialUi.RaisedButton primary={true} onMouseDown={this.onPrintButtonClick} label='Print'/>
-                </MaterialUi.TableRowColumn>
-            </MaterialUi.TableRow>
-        );
-    }
-
-});
-
 var OrderList = React.createClass({
 
     render: function() {
         var that = this;
         var orderNodes = this.props.orders.map(function (order) {
             return (
-                <OrderListItem order={order} key={order.id} updateQR={that.props.updateQR}></OrderListItem>
+              <MaterialUi.TableRow className='order-list-item' selected={order.id === that.props.selectedOrderID} key={order.id}>
+                  <MaterialUi.TableRowColumn className='order-name'>{order.name}</MaterialUi.TableRowColumn>
+                  <MaterialUi.TableRowColumn className='order-location'>{order.location}</MaterialUi.TableRowColumn>
+                  <MaterialUi.TableRowColumn className='order-food-type'>{order.food_type}</MaterialUi.TableRowColumn>
+                  <MaterialUi.TableRowColumn className='order-phone-number'>{order.phone_number}</MaterialUi.TableRowColumn>
+              </MaterialUi.TableRow>
             );
         });
 
+        var style = {
+          margin: 6,
+        };
+
         return (
-            <MaterialUi.Table>
-                <MaterialUi.TableHeader adjustForCheckbox={false} displaySelectAll={false} enableSelectAll={false}>
+            <MaterialUi.Table selectable={true} onRowSelection={this.props.updateSelectedOrder}>
+                <MaterialUi.TableHeader adjustForCheckbox={true} displaySelectAll={true} enableSelectAll={false}>
+                    <MaterialUi.TableRow>
+                      <MaterialUi.TableHeaderColumn colSpan="4" style={{textAlign: 'right'}}>
+                        <MaterialUi.RaisedButton disabled={!this.props.enableButtons} style={style} primary={true} onMouseDown={this.props.onPrintButtonClick} label='Print'/>
+                        <MaterialUi.RaisedButton disabled={!this.props.enableButtons} style={style} secondary={true} onMouseDown={this.props.onArchiveButtonClick} label='Archive'/>
+                      </MaterialUi.TableHeaderColumn>
+                    </MaterialUi.TableRow>
                     <MaterialUi.TableRow>
                         <MaterialUi.TableHeaderColumn>Name</MaterialUi.TableHeaderColumn>
                         <MaterialUi.TableHeaderColumn>Location</MaterialUi.TableHeaderColumn>
                         <MaterialUi.TableHeaderColumn>Food Type</MaterialUi.TableHeaderColumn>
                         <MaterialUi.TableHeaderColumn>Phone Number</MaterialUi.TableHeaderColumn>
-                        <MaterialUi.TableHeaderColumn>Print</MaterialUi.TableHeaderColumn>
                     </MaterialUi.TableRow>
                 </MaterialUi.TableHeader>
-                <MaterialUi.TableBody>
+                <MaterialUi.TableBody displayRowCheckbox={true} showRowHover={true}>
                   { orderNodes }
                 </MaterialUi.TableBody>
             </MaterialUi.Table>
@@ -82,7 +60,35 @@ var OrderList = React.createClass({
 
 var OrderApp = React.createClass({
     getInitialState: function() {
-        return { orders: [], qr_code: "" };
+        return { 
+          orders: [], 
+          qr_code: "",
+          selectedOrderID: null,
+          selectedOrderIndex: null
+        };
+    },
+
+    updateSelectedOrder: function(selectedRows) {
+      var rowNum = selectedRows[0];
+      var order = this.state.orders[rowNum];
+
+      if (order) {
+        var data = [order.name, order.phone_number, order.location, order.food_type];
+        var message = data.join(',');
+
+        this.setState({
+          selectedOrderIndex: rowNum,
+          selectedOrderID: order.id,
+          qr_code: message
+        });
+      } 
+      else {
+        this.setState({
+          selectedOrderIndex: null,
+          selectedOrderID: null,
+          qr_code: ""
+        });
+      }
     },
 
     updateQRCode: function(message) {
@@ -90,6 +96,29 @@ var OrderApp = React.createClass({
       this.setState({
         orders: this.state.orders,
         qr_code: this.state.qr_code
+      });
+    },
+
+    onPrintButtonClick: function() {
+      var order = this.state.orders[this.state.selectedOrderIndex];
+      message = {
+        to: order.phone_number,
+        body: order.name + ", your delivery of " + order.food_type + " is on its way!"
+      };
+      console.log(message);
+      notifsRef.push(message);
+
+      window.print();
+    },
+
+    onArchiveButtonClick: function() {
+      ordersRef.child(this.state.selectedOrderID).remove();
+      this.state.orders.splice(this.state.selectedOrderIndex, 1);
+      this.setState({
+        orders: this.state.orders,
+        selectedOrderIndex: null,
+        selectedOrderID: null,
+        qr_code: ""
       });
     },
 
@@ -116,7 +145,7 @@ var OrderApp = React.createClass({
           <MuiThemeProvider muiTheme={muiTheme}>
             <div>
               <div className="order-list">
-                <OrderList orders={this.state.orders} updateQR={this.updateQRCode}/>
+                <OrderList orders={this.state.orders} onPrintButtonClick={this.onPrintButtonClick} onArchiveButtonClick={this.onArchiveButtonClick} updateSelectedOrder={this.updateSelectedOrder} selectedOrderID={this.state.selectedOrderID} enableButtons={this.state.selectedOrderID !== null}/>
               </div>
               <div className="qr-code">
                 <QRCode value={this.state.qr_code} size={128}/>
